954c9e8eb6f58f73aefc7fa6729b8eaf1e28194f
Bamdad Dashtban <bamdadd@gmail.com>
2023-11-19 17:30:56 +0000

    adding Gas reporter

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,7 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomiclabs/hardhat-ethers";
 import '@nomiclabs/hardhat-waffle';
+import 'hardhat-gas-reporter';
 
 const config: HardhatUserConfig = {
   solidity: "0.8.19",
