d661fbf6fe282bb11deb279644e6fffa140c9a05
Bamdad Dashtban <bamdadd@gmail.com>
2023-11-19 17:32:28 +0000

    adding solidity coverage

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,6 +2,7 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomiclabs/hardhat-ethers";
 import '@nomiclabs/hardhat-waffle';
 import 'hardhat-gas-reporter';
+import 'solidity-coverage'
 
 const config: HardhatUserConfig = {
   solidity: "0.8.19",
