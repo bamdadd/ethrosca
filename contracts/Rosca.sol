50df944a99436bd6d2e8108f774c5581d5b204aa
Bamdad Dashtban <bamdadd@gmail.com>
2023-11-20 16:01:32 +0000

    making addresses dynamic

--- contracts/Rosca.sol
+++ contracts/Rosca.sol
@@ -2,31 +2,44 @@
 pragma solidity ^0.8.0;
 
 contract ROSCA {
-    address[10] public members;
     uint256 public constant contribution = 1 ether;
     uint8 public currentMonth = 0;
+    address[] public members;
+    mapping(address => bool) public hasDeposited;
     mapping(address => bool) public hasWithdrawn;
 
     event Withdrawal(address indexed member, uint256 amount);
 
-
-    constructor(address[10] memory _members) {
-        members = _members;
-    }
-
     function contribute() external payable {
         require(msg.value == contribution, "Contribution must be exactly 1 ETH");
         require(!hasWithdrawn[msg.sender], "Already withdrawn");
+
+        if (!hasDeposited[msg.sender]) {
+            members.push(msg.sender);
+            hasDeposited[msg.sender] = true;
+        }
     }
 
     function withdraw() external {
-        require(msg.sender == members[currentMonth], "Not your turn to withdraw");
-        require(address(this).balance >= 1 ether, "Not enough balance");
-        (bool sent, ) = payable(msg.sender).call{value: 1 ether}("");
+        require(hasDeposited[msg.sender], "Not a member or didn't contribute");
+        require(!hasWithdrawn[msg.sender], "Already withdrawn");
+        require(currentMonth < members.length, "All withdrawals done");
+
+        address member = members[currentMonth];
+        require(msg.sender == member, "Not your turn to withdraw");
+
+        (bool sent, ) = payable(member).call{value: 1 ether}("");
         require(sent, "Failed to send Ether");
-        emit Withdrawal(msg.sender, 1 ether);
-        hasWithdrawn[msg.sender] = true;
+
+        hasWithdrawn[member] = true;
         currentMonth++;
+
+        emit Withdrawal(msg.sender, 1 ether);
+    }
+
+    function getMembers() external view returns (address[] memory) {
+        return members;
+
     }
 
 }
--- test/Rosca.test.ts
+++ test/Rosca.test.ts
@@ -6,18 +6,20 @@ describe("ROSCA Contract", function () {
     let rosca: Contract;
     let signers: Signer[];
 
-    before(async function () {
+    beforeEach(async function () {
         signers = await ethers.getSigners();
         const ROSCA = await ethers.getContractFactory("ROSCA");
-        rosca = await ROSCA.deploy(signers.slice(0, 10).map((s) => s.getAddress()));
+        rosca = await ROSCA.deploy();
     });
 
     it("Should simulate 10 months of ROSCA for 1 ETH", async function () {
         // Each member contributes 1 ETH
         for (const signer of signers.slice(0, 10)) {
-            await rosca
+            const response = await rosca
                 .connect(signer)
                 .contribute({value: ethers.utils.parseEther("1")});
+            expect(response).to.emit(rosca, "Contribution");
+
         }
 
         // Simulate 10 months
@@ -31,4 +33,39 @@ describe("ROSCA Contract", function () {
             );
         }
     });
+
+    it("Should simulate 11 users contributing to the ROSCA contract", async function () {
+        // Each member contributes 1 ETH
+        const membersCount = 11;
+        for (const signer of signers.slice(0, membersCount)) {
+
+            const response = await rosca
+                .connect(signer)
+                .contribute({value: ethers.utils.parseEther("1")});
+            expect(response).to.emit(rosca, "Contribution");
+        }
+
+        // check contract balance
+        expect(await ethers.provider.getBalance(rosca.address)).to.equal(
+            ethers.utils.parseEther(membersCount.toString())
+        );
+
+        // get membersCount of members
+        const members = await rosca.getMembers();
+        expect(members.length).to.equal(membersCount);
+
+        for (let i = 0; i < membersCount; i++) {
+            const tx = await rosca.connect(signers[i]).withdraw();
+            await expect(tx)
+                .to.emit(rosca, "Withdrawal")
+                .withArgs(await signers[i].getAddress(), ethers.utils.parseEther("1"));
+
+            expect(await ethers.provider.getBalance(rosca.address)).to.equal(
+                ethers.utils.parseEther(`${membersCount - i -1}`)
+            );
+        }
+
+        await expect(rosca.connect(signers[0]).withdraw()).to.be.revertedWith("Already withdrawn");
+    });
+
 });
