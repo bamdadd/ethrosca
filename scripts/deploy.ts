c713a8d320cc954151cd5caeeb92c14cb5987269
Bamdad Dashtban <bamdadd@gmail.com>
2023-11-20 14:23:20 +0000

    Updating deploy

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -7,7 +7,7 @@ async function main() {
 
   const lockedAmount = parseEther("0.001");
 
-  const lock = await hre.viem.deployContract("Lock", [unlockTime], {
+  const lock = await hre.viem.deployContract("ROSCA", [unlockTime], {
     value: lockedAmount,
   });
 
